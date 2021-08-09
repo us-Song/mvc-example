53a40a9633399577ab6abbdbd4c422725f1886a2
us-Song <dmltjr05@naver.com>
2021-08-09 14:01:16 +0900

    초기 커밋

---
+++ mvcpatternex.h
@@ -0,0 +1,32 @@
+﻿
+// mvcpatternex.h: PROJECT_NAME 애플리케이션에 대한 주 헤더 파일입니다.
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "PCH에 대해 이 파일을 포함하기 전에 'pch.h'를 포함합니다."
+#endif
+
+#include "resource.h"		// 주 기호입니다.
+
+
+// CmvcpatternexApp:
+// 이 클래스의 구현에 대해서는 mvcpatternex.cpp을(를) 참조하세요.
+//
+
+class CmvcpatternexApp : public CWinApp
+{
+public:
+	CmvcpatternexApp();
+
+// 재정의입니다.
+public:
+	virtual BOOL InitInstance();
+
+// 구현입니다.
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CmvcpatternexApp theApp;
